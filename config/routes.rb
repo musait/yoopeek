c5f76e65a1c6c63612c898df0d0abe77e7c9576d
Mustapha <ait.mus06@gmail.com>
2019-09-25 17:24:27 +0200

    add i18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'home/index'
-  get 'home/private'
-  root 'home#index'
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    devise_for :users
+    get 'home/private'
+    root 'home#index'
 
-  resources :quotes
-  resources :jobs
-  get 'show' => 'jobs#show_test'
-  get 'show_quote' => 'quotes#show_test'
+    resources :quotes
+    resources :jobs
+    get 'show' => 'jobs#show_test'
+    get 'show_quote' => 'quotes#show_test'
 
-  namespace :admin do
-    get "index" => "home#index"
+    namespace :admin do
+      get "index" => "home#index"
+    end
   end
+
 end
