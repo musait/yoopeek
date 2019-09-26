08c9c4ca4df76c7a6989ddd0026f76db22651729
Pierre-Quentin Deguingand <pierrequentindeguingand@gmail.com>
2019-09-26 11:57:34 +0200

    added facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
-    devise_for :users
-    get 'home/private'
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
     root 'home#index'
 
     resources :quotes
--- db/migrate/20190924102552_devise_create_users.rb
+++ db/migrate/20190924102552_devise_create_users.rb
@@ -32,6 +32,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      ## Omniauth
+      t.string  :facebook_id
+      t.string  :google_id
+
 
       t.timestamps null: false
     end
