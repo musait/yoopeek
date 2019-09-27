72d7beb4c352c956c619605f2aa40b9948d41c63
Mustapha <ait.mus06@gmail.com>
2019-09-27 11:16:13 +0200

    change route for social login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, only: :omniauth_callbacks, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
+
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
-  devise_for :users, controllers: {
-    omniauth_callbacks: 'users/omniauth_callbacks'
-  }
+    devise_for :users, skip: :omniauth_callbacks
     root 'home#index'
 
     resources :quotes
