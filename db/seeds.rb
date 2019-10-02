5b1d04d47a68890800883f1bcda882869a4396eb
Pierre-Quentin Deguingand <pierrequentindeguingand@gmail.com>
2019-10-02 09:41:36 +0200

    seeding db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create([{ name: 'Photographie' }, { name: 'Maquillage' }, { name: 'Vidéographie' }])
+UnderCategory.create([{ name: 'Portrait', category_id: Category.find_by(name: 'Photographie') },
+  { name: 'Évènement', category_id: Category.find_by(name: 'Photographie') },
+  { name: 'Mariage', category_id: Category.find_by(name: 'Photographie') },
+  { name: 'Cinéma', category_id: Category.find_by(name: 'Maquillage') },
+  { name: 'Mariage', category_id: Category.find_by(name: 'Maquillage') },
+  { name: 'Évènement', category_id: Category.find_by(name: 'Vidéographie') },
+  { name: 'Mariage', category_id: Category.find_by(name: 'Vidéographie') },
+  { name: 'Court-métrage', category_id: Category.find_by(name: 'Vidéographie') },
+  { name: 'Publicité', category_id: Category.find_by(name: 'Vidéographie') },
+  ])
+User.create([
+  { email: 'a@a.com', firstname: 'John', lastname: 'Doe', type: 'User', nationality: 'French', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nisl ex, ornare accumsan enim in, ultricies venenatis risus. Vivamus sagittis est consectetur molestie molestie.'},
+  { email: 'b@b.com', firstname: 'Will', lastname: 'Pioneer', type: 'Worker', PriceRate: '40', nationality: 'English', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nisl ex, ornare accumsan enim in, ultricies venenatis risus. Vivamus sagittis est consectetur molestie molestie.'}
+  ])
