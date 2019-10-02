8442939edd4f7793741d5d00d91f7c3ebbd08e41
Mustapha <ait.mus06@gmail.com>
2019-10-02 09:52:35 +0200

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,16 @@
 
 Category.create([{ name: 'Photographie' }, { name: 'Maquillage' }, { name: 'Vidéographie' }])
 UnderCategory.create([{ name: 'Portrait', category_id: Category.find_by(name: 'Photographie') },
-  { name: 'Évènement', category_id: Category.find_by(name: 'Photographie') },
-  { name: 'Mariage', category_id: Category.find_by(name: 'Photographie') },
-  { name: 'Cinéma', category_id: Category.find_by(name: 'Maquillage') },
-  { name: 'Mariage', category_id: Category.find_by(name: 'Maquillage') },
-  { name: 'Évènement', category_id: Category.find_by(name: 'Vidéographie') },
-  { name: 'Mariage', category_id: Category.find_by(name: 'Vidéographie') },
-  { name: 'Court-métrage', category_id: Category.find_by(name: 'Vidéographie') },
-  { name: 'Publicité', category_id: Category.find_by(name: 'Vidéographie') },
+  { name: 'Évènement', category_id: Category.find_by(name: 'Photographie').id },
+  { name: 'Mariage', category_id: Category.find_by(name: 'Photographie').id },
+  { name: 'Cinéma', category_id: Category.find_by(name: 'Maquillage').id },
+  { name: 'Mariage', category_id: Category.find_by(name: 'Maquillage').id },
+  { name: 'Évènement', category_id: Category.find_by(name: 'Vidéographie').id },
+  { name: 'Mariage', category_id: Category.find_by(name: 'Vidéographie').id },
+  { name: 'Court-métrage', category_id: Category.find_by(name: 'Vidéographie').id },
+  { name: 'Publicité', category_id: Category.find_by(name: 'Vidéographie').id },
   ])
 User.create([
-  { email: 'a@a.com', firstname: 'John', lastname: 'Doe', type: 'User', nationality: 'French', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nisl ex, ornare accumsan enim in, ultricies venenatis risus. Vivamus sagittis est consectetur molestie molestie.'},
-  { email: 'b@b.com', firstname: 'Will', lastname: 'Pioneer', type: 'Worker', PriceRate: '40', nationality: 'English', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nisl ex, ornare accumsan enim in, ultricies venenatis risus. Vivamus sagittis est consectetur molestie molestie.'}
+  { email: 'a@a.com', firstname: 'John', lastname: 'Doe', type: 'User', nationality: 'French', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nisl ex, ornare accumsan enim in, ultricies venenatis risus. Vivamus sagittis est consectetur molestie molestie.',password:"password"},
+  { email: 'b@b.com', firstname: 'Will', lastname: 'Pioneer', type: 'Worker', price_rate: '40', nationality: 'English', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nisl ex, ornare accumsan enim in, ultricies venenatis risus. Vivamus sagittis est consectetur molestie molestie.'password:"password"}
   ])
