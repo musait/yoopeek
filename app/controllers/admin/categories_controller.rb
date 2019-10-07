e594ceccb22f6374aa34527c549c38b74512b46d
Mustapha <ait.mus06@gmail.com>
2019-10-07 09:16:31 +0200

    change some routes, access, variable ..

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,5 @@
 class Admin::CategoriesController <  AdminController
-  before_action :set_category, only: [:show]
+  before_action :set_category, only: [:show,:edit,:destroy]
 
   # GET /categories
   # GET /categories.json
@@ -38,7 +38,7 @@ class Admin::CategoriesController <  AdminController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
+        format.html { redirect_to admin_categories_path, notice: 'Category was successfully created.' }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new }
@@ -53,7 +53,7 @@ class Admin::CategoriesController <  AdminController
     respond_to do |format|
       if @category.update(category_params)
         format.html { redirect_to @category, notice: 'Category was successfully updated.' }
-        format.json { render :show, status: :ok, location: @category }
+        format.json { render :index, status: :ok, location: @category }
       else
         format.html { render :edit }
         format.json { render json: @category.errors, status: :unprocessable_entity }
@@ -66,7 +66,7 @@ class Admin::CategoriesController <  AdminController
   def destroy
     @category.destroy
     respond_to do |format|
-      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
+      format.html { redirect_to admin_categories_path, notice: 'La catégorie a été supprimée' }
       format.json { head :no_content }
     end
   end
@@ -79,6 +79,6 @@ class Admin::CategoriesController <  AdminController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def category_params
-      params.require(:category).permit(:name)
+      params.permit(:name)
     end
 end
