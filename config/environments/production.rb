b75bf133fb0d87db754bc6c86a9ce08d6618f13b
Mustapha <ait.mus06@gmail.com>
2019-10-09 18:05:04 +0200

    try to fix ssl conection

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,8 +92,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { :host => 'https://yoopeek.heroku.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     :user_name => Rails.application.credentials.sendgrid[:sendgrid_username],
     :password => Rails.application.credentials.sendgrid[:sendgrid_password],
