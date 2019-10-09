7dfea4c43707164a6cbd156e02c61caee2c8596e
Mustapha <ait.mus06@gmail.com>
2019-10-09 17:46:55 +0200

    disable ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+   config.force_ssl = false
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -97,7 +97,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :user_name => Rails.application.credentials.sendgrid[:sendgrid_username],
     :password => Rails.application.credentials.sendgrid[:sendgrid_password],
-    :domain => 'yourdomain.com',
+    :domain => 'www.yoopeek.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
