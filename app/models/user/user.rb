e36d5f089ae74307a55158209009df80b0102720
Mustapha <ait.mus06@gmail.com>
2019-10-11 17:51:42 +0200

    add method to find rooms for user

--- app/models/user/user.rb
+++ app/models/user/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   devise :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   self.inheritance_column = :type
-  has_many :authored_rooms, class_name: 'Rooms', foreign_key: 'author_id'
-  has_many :received_rooms, class_name: 'Rooms', foreign_key: 'received_id'
+  has_many :authored_rooms, class_name: 'Room', foreign_key: 'author_id'
+  has_many :received_rooms, class_name: 'Room', foreign_key: 'receiver_id'
   has_many :room_messages
   acts_as_reader
 
@@ -17,6 +17,10 @@ class User < ApplicationRecord
   end
   def self.types
     %w(Worker Customer)
+  end
+
+  def have_one_room_with_user(user)
+    authored_rooms.with_receiver(user) || received_rooms.with_author(user)
   end
 
   def self.from_facebook(auth)
