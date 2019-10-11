f79906b34499800bab3b19144c84c0398a719809
Mustapha <ait.mus06@gmail.com>
2019-10-11 11:29:27 +0200

    delete avatar

--- app/assets/javascripts/channels/room_channel.js
+++ app/assets/javascripts/channels/room_channel.js
@@ -3,8 +3,7 @@ $(function() {
     var $element = $(element),
         room_id = $element.data('room-id')
         messageTemplate = $('[data-role="message-template"]');
-
-    $element.animate({ scrollTop: $element.prop("scrollHeight")}, 1000);
+        $element.animate({ scrollTop: $element.prop("scrollHeight")}, 1000);
 
     App.cable.subscriptions.create(
       {
@@ -14,7 +13,6 @@ $(function() {
       {
         received: function(data) {
           var content = messageTemplate.children().clone(true, true);
-          content.find('[data-role="user-avatar"]').attr('src', data.user_avatar_url);
           content.find('[data-role="message-text"]').text(data.message);
           content.find('[data-role="message-date"]').text(data.updated_at);
           $element.append(content);
