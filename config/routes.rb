47a5e29244c880265f028947428b0790b5cc3045
Mustapha <ait.mus06@gmail.com>
2019-10-13 14:21:32 +0200

    reorganization of route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,43 +1,41 @@
 Rails.application.routes.draw do
 
 
-  resources :quote_elements
-  resources :room_messages
-  resources :rooms
+
   devise_for :users, only: :omniauth_callbacks, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
 
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
-  devise_for :users, skip: :omniauth_callbacks
-  root 'home#index'
-  mount ActionCable.server, at: '/cable'
+    devise_for :users, skip: :omniauth_callbacks
+    root 'home#index'
+    mount ActionCable.server, at: '/cable'
 
-  resources :quotes
-  resources :jobs, param: :slug
-  get 'show' => 'jobs#show_test'
-  get 'show_quote' => 'quotes#show_test'
+    resources :rooms
+    resources :room_messages
+    resources :quotes
+    resources :quote_elements
+    resources :jobs, param: :slug
+    get 'show' => 'jobs#show_test'
+    get 'show_quote' => 'quotes#show_test'
 
-  resources :reviews
-  resources :subcategories
-  resources :categories do
-    member do
-      get :get_subcategories, defaults: { format: "js" }
+    resources :reviews
+    resources :subcategories
+    resources :categories do
+      member do
+        get :get_subcategories, defaults: { format: "js" }
+      end
+    end
+    resources :companies
+    namespace :admin do
+      get "index" => "home#index"
+      resources :jobs, param: :slug
+      resources :categories
+      resources :subcategories
+      resources :reviews
+      resources :quotes
+      resources :format_deliveries
+      resources :users
+      resources :professions
     end
   end
-  resources :companies
-  resources :room_messages
-  resources :rooms
-
-  namespace :admin do
-    get "index" => "home#index"
-    resources :jobs, param: :slug
-    resources :categories
-    resources :subcategories
-    resources :reviews
-    resources :quotes
-    resources :format_deliveries
-    resources :users
-    resources :professions
-  end
-end
 
 end
