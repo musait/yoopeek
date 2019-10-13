ea3be4db5147d9e479a713079cb70533c87358ad
Mustapha <ait.mus06@gmail.com>
2019-10-13 14:25:56 +0200

    add parameters

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -73,6 +73,6 @@ class QuotesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def quote_params
-      params.require(:quote).permit(:name,quote_elements_attributes: [ :id, :content,:quantity,:total, :price, :_destroy ])
+      params.require(:quote).permit(:name,:job_id,:quote_elements_attributes => [ :id, :content,:quantity,:total, :price, :_destroy ])
     end
 end
