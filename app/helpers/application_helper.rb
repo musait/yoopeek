4acf3cfd3b186894577274dead4db16d7e7b4e1c
Mustapha <ait.mus06@gmail.com>
2019-10-13 14:25:46 +0200

    delete helper because of the gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,36 +7,4 @@ module ApplicationHelper
   def have_one_room_with_user(user)
     authored_rooms.with_receiver(user).first ||received_rooms_with_author(user).first
   end
-  def link_to_add_fields(name = nil, f = nil, association = nil, options = nil, html_options = nil, &block)
-  # If a block is provided there is no name attribute and the arguments are
-  # shifted with one position to the left. This re-assigns those values.
-  f, association, options, html_options = name, f, association, options if block_given?
-
-  options = {} if options.nil?
-  html_options = {} if html_options.nil?
-
-  if options.include? :locals
-    locals = options[:locals]
-  else
-    locals = { }
-  end
-
-  if options.include? :partial
-    partial = options[:partial]
-  else
-    partial = association.to_s.singularize
-  end
-
-  # Render the form fields from a file with the association name provided
-  new_object = association.to_s.classify.safe_constantize.new
-  fields = f.fields_for(association, new_object, child_index: 'new_record') do |builder|
-    render(partial, locals.merge!( f: builder))
-  end
-
-  # The rendered fields are sent with the link within the data-form-prepend attr
-  html_options['data-form-prepend'] = raw CGI::escapeHTML( fields )
-  html_options['href'] = '#'
-
-  content_tag(:a, name, html_options, &block)
-end
 end
