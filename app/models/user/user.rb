0ce7a27e93b7c93395ba3676b0b0ff50e0027497
Mustapha <ait.mus06@gmail.com>
2019-10-16 14:35:59 +0200

    add address to model

--- app/models/user/user.rb
+++ app/models/user/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
   has_many :authored_rooms, class_name: 'Room', foreign_key: 'author_id'
   has_many :received_rooms, class_name: 'Room', foreign_key: 'receiver_id'
   has_many :room_messages
+  belongs_to :address
+  accepts_nested_attributes_for :address
   acts_as_reader
 
   def add_type
--- app/models/user/worker.rb
+++ app/models/user/worker.rb
@@ -1,5 +1,6 @@
 class Worker < User
   has_one :company, dependent: :destroy
+  accepts_nested_attributes_for :company
   has_many :reviews, dependent: :destroy
   has_many :jobs
   belongs_to :profession
