1942b3353bf0ff79dbccdcedf020bda6e22d4e2e
Mustapha <ait.mus06@gmail.com>
2019-10-16 14:36:12 +0200

    add address and validation to company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,7 @@
 class Company < ApplicationRecord
   belongs_to :worker, :class_name => "Worker", :foreign_key => "worker_id"
+  belongs_to :address
+  accepts_nested_attributes_for :address
+  validates_with IbanValidator, if: -> {!self.iban.empty?}
+  validates :vat_number, :valvat => {:lookup => true,:allow_blank => true}
 end
