8948141f4ca4a02bdd68cfbc6dfc40174dca3a8b
Mustapha <ait.mus06@gmail.com>
2019-10-16 14:35:45 +0200

    add iban validator class

---
+++ app/validators/iban_validator.rb
@@ -0,0 +1,9 @@
+require 'iban-tools'
+
+class IbanValidator < ActiveModel::Validator
+  def validate(record)
+    unless IBANTools::IBAN.valid?(record.iban)
+      record.errors.add(:base, I18n.t("errors.messages.company.iban_not_valid"))
+    end
+  end
+end
