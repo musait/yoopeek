145c9303bfcb39b9964e0314a9dd2d93349dc0f8
Mustapha <ait.mus06@gmail.com>
2019-10-17 17:15:45 +0200

    add defaut name to company

--- db/migrate/20190930160705_create_companies.rb
+++ db/migrate/20190930160705_create_companies.rb
@@ -2,7 +2,7 @@ class CreateCompanies < ActiveRecord::Migration[5.2]
   def change
     create_table :companies, id: :uuid do |t|
 
-      t.string :name
+      t.string :name, default: " "
       t.string :iban
       t.string :bic
       t.string :bank_name
