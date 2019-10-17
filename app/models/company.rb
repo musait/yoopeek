d8e7d117c15e16d48e613e18200fa58338af4d27
Mustapha <ait.mus06@gmail.com>
2019-10-17 17:17:56 +0200

    add address after create company

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,6 +2,12 @@ class Company < ApplicationRecord
   belongs_to :worker, :class_name => "Worker", :foreign_key => "worker_id"
   belongs_to :address
   accepts_nested_attributes_for :address
-  validates_with IbanValidator, if: -> {!self.iban.empty?}
+  validates_with IbanValidator, if: -> {!self.iban.blank?}
   validates :vat_number, :valvat => {:lookup => true,:allow_blank => true}
+  after_create :create_address
+
+  def create_address
+    self.build_address
+    self.save
+  end
 end
