17172ccb297b41b82b7c68a3187b6d294909b445
Mustapha <ait.mus06@gmail.com>
2019-10-17 17:19:11 +0200

    add condition to create customer or worker after a social login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,9 +1,18 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def facebook
-    @user = User.from_facebook(request.env['omniauth.auth'])
+    if request.env['omniauth.params']
+      @user = Worker.from_facebook(request.env['omniauth.auth'],request.env['omniauth.params'])
+    else
+      @user = Customer.from_facebook(request.env['omniauth.auth'],request.env['omniauth.params'])
+    end
     if @user.persisted?
-      sign_in_and_redirect @user, event: :authentification
+      if @user.is_worker && !@user.approved?
+        sign_in(@user)
+        redirect_to edit_user_registration_path, notice: 'Veuillez remplir les champs conçernant votre profil et celui de votre entreprise afin que votre inscription en tant que professionel soit pris en compte'
+      else
+        sign_in_and_redirect @user, event: :authentification
+      end
     else
       session['devise.facebook'] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
@@ -11,22 +20,24 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def google_oauth2
-    # auth = request.env["omniauth.auth"]
-    # user = User.where(provider: auth["provider"], uid: auth["uid"]).first_or_initialize(email: auth["info"]["email"])
-    # user.name ||= auth["info"]["name"]
-    # user.save!
-    #
-    # user.remember_me = true
-    # sign_in(:user, user)
-    #
-    # redirect_to after_sign_in_path_for(user)
-    @user = User.from_google(request.env['omniauth.auth'])
+    if request.env['omniauth.params']
+      @user = Worker.from_facebook(request.env['omniauth.auth'],request.env['omniauth.params'])
+    else
+      @user = Customer.from_facebook(request.env['omniauth.auth'],request.env['omniauth.params'])
+    end
     if @user.persisted?
-      sign_in_and_redirect @user, event: :authentification
+      if @user.is_worker && !@user.approved?
+        sign_in(@user)
+        redirect_to edit_user_registration_path, notice: 'Veuillez remplir les champs conçernant votre profil et celui de votre entreprise afin que votre inscription en tant que professionel soit pris en compte'
+      else
+        sign_in_and_redirect @user, event: :authentification
+      end
     else
       session['devise.google_oauth2'] = request.env['omniauth.auth']
       redirect_to new_user_registration_url
     end
   end
+
+
 
 end
