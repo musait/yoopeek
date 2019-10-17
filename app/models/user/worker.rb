184da50888e500b17b4e7327455ac2c1e2e8cada
Mustapha <ait.mus06@gmail.com>
2019-10-17 17:17:04 +0200

    add build_company after user create

--- app/models/user/worker.rb
+++ app/models/user/worker.rb
@@ -1,7 +1,8 @@
 class Worker < User
   has_one :company, dependent: :destroy
+  before_create :build_company
   accepts_nested_attributes_for :company
   has_many :reviews, dependent: :destroy
   has_many :jobs
-  belongs_to :profession
+  belongs_to :profession, optional: true
 end
