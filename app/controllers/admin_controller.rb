214277e360a45636e9c0cdaafec8be6ee6597d19
Mustapha <ait.mus06@gmail.com>
2019-10-20 13:12:11 +0200

    add method to check if user is admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,29 +1,27 @@
 class AdminController < ApplicationController
   layout'admin'
   before_action :set_database
-  before_action :require_admin
+  before_action :is_admin?
 
   def index
 
   end
-  # Methods omitted
 
-  # def require_admin
-  #   unless current_user.admin?
-  #     redirect_to root_path
-  #   end
-  # end
   def set_database
     @jobs = Job.all
     @categories = Category.all
     @users = User.all
   end
-  def require_admin
-    if current_user
-      redirect_to root_path, alert: "Vous devez être administrateur pour vous rendre sur cette page" unless current_user.admin?
+
+  def is_admin?
+    if current_user.present?
+      if current_user.is_admin
+      else
+        redirect_to root_path, alert: "Vous devez être administrateur pour vous rendre sur cette page"
+      end
     else
       redirect_to new_user_session_path, alert: "Vous devez être connecté et être administrateur pour vous rendre sur cette page"
-
     end
   end
+
 end
