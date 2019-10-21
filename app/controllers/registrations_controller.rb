7bdfab3a57fa474fea964b4a3a01c9bc1d6e7fcc
Mustapha <ait.mus06@gmail.com>
2019-10-21 19:23:50 +0200

    commit test

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,21 @@
 class RegistrationsController < Devise::RegistrationsController
   skip_before_action :check_if_approved, only: [:edit, :update]
 
-  def update
+  def create
     super
+    if params[:user][:is_worker] == "1"
+      @user.update(type: "Worker")
+    else
+      @user.update(type: "Customer")
+    end
+    if @user.persisted?
+      if @user.is_worker && !@user.approved?
+        sign_in(@user)
+        redirect_to edit_user_registration_path, notice: 'Veuillez remplir les champs conçernant votre profil et celui de votre entreprise afin que votre inscription en tant que professionel soit pris en compte'
+      else
+        sign_in_and_redirect @user, event: :authentification
+      end
+    end
   end
   protected
 
--- app/models/user/user.rb
+++ app/models/user/user.rb
@@ -15,6 +15,7 @@ class User < ApplicationRecord
   accepts_nested_attributes_for :address
   after_create :send_admin_mail
   acts_as_reader
+  phony_normalize :phone_number, default_country_code: 'FR'
 
   def add_type
     self.type ="Customer"
