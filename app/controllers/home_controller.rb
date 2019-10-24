fb11f5a9fadcf37dcd1ff52f4c9c242452343259
Cruxys <destin.ludovick@gmail.com>
2019-10-24 19:18:20 +0200

    CI: remove raw url add active storage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,14 @@
 class HomeController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :search_result]
   def index
+  end
+  def search_result
+    if params[:search].present?
+      @workers = Worker.joins(:categories).where(categories: {name: params[:name]})
+    else
+      @workers = Worker.paginate(page: params[:page], per_page: params[:per_page])
+    end
+
   end
 
   def private
