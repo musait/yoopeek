16b4e53973b9dc5f7df7eae8d630b085bb04a32d
Mustapha <ait.mus06@gmail.com>
2019-10-25 18:43:00 +0200

    change type of columns

--- db/migrate/20191009113056_create_quote_elements.rb
+++ db/migrate/20191009113056_create_quote_elements.rb
@@ -2,9 +2,9 @@ class CreateQuoteElements < ActiveRecord::Migration[5.2]
   def change
     create_table :quote_elements, id: :uuid do |t|
       t.text :content
-      t.string :quantity
-      t.string :price
-      t.string :total
+      t.integer :quantity
+      t.integer :price
+      t.integer :total
       t.references :quote, type: :uuid, foreign_key: true
       t.timestamps
     end
