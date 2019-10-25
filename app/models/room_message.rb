b2d83d89646accedae67e78fdc738a1e673ffa5c
Mustapha <ait.mus06@gmail.com>
2019-10-25 18:46:40 +0200

    delete useless method

--- app/models/room_message.rb
+++ app/models/room_message.rb
@@ -1,5 +1,5 @@
 class RoomMessage < ApplicationRecord
-  belongs_to :room, inverse_of: :room_messages
+  belongs_to :room
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :receiver, class_name: 'User', foreign_key: 'receiver_id'
   acts_as_readable on: :created_at
