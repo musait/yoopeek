647d6acfd576b16dd47f221803f3eb04e1e7393d
Mustapha <ait.mus06@gmail.com>
2019-10-27 12:42:44 +0100

    add relation between quote and user through sender_id and receiver id

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,14 +2,15 @@ class Quote < ApplicationRecord
   has_many :quote_elements
   accepts_nested_attributes_for :quote_elements,:reject_if => :all_blank, :allow_destroy => true
   belongs_to :job
-  belongs_to :user
-  has_one :notification
-  
+  belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
+  belongs_to :receiver, class_name: 'User', foreign_key: 'receiver_id'
+  has_many :notifications
+
   enum status: [:created, :declined, :accepted]
 
   after_create :create_notification
 
   def create_notification
-    Notification.create_for user, self
+    Notification.create_for sender,receiver, self
   end
 end
