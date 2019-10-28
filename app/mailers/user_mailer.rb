8ad120294fe0165bfe5915a5b4012b71679830bd
Mustapha <ait.mus06@gmail.com>
2019-10-28 11:06:57 +0100

    add traduction for mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,12 +5,12 @@ class UserMailer < Devise::Mailer
     def new_quote
       @user = params[:user]
       @quote = params[:quote]
-      mail(to: @user.email, subject: 'You have a new quote')
+      mail(to: @user.email, subject: default_i18n_subject(user: @user.full_name))
     end
 
     def quote_declined
       @user = params[:user]
       @quote = params[:quote]
-      mail(to: @user.email, subject: 'You have a new quote')
+      mail(to: @user.email, subject: default_i18n_subject(user: @user.full_name))
     end
   end
