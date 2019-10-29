e0ce621d5a91874c76150ef7163e327342ab3858
Mustapha <ait.mus06@gmail.com>
2019-10-29 22:36:47 +0100

    create portfolio model

---
+++ app/models/portfolio.rb
@@ -0,0 +1,14 @@
+class Portfolio < ApplicationRecord
+  belongs_to :user
+  has_many_attached :picture
+
+  def picture_url
+    if picture.attached?
+      ActiveStorage::Current.set(host: Rails.application.credentials.dig(Rails.env.to_sym, :host)) do
+        picture[0].service_url
+      end
+    else
+      ActionController::Base.helpers.asset_path 'logo.png'
+    end
+  end
+end
