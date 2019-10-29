e94cdf3c960104266014c37f2d020406405abcb0
Mustapha <ait.mus06@gmail.com>
2019-10-29 22:36:37 +0100

    add destroy dependent btw quote_element and quotes

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,5 @@
 class Quote < ApplicationRecord
-  has_many :quote_elements
+  has_many :quote_elements, dependent: :destroy
   accepts_nested_attributes_for :quote_elements,:reject_if => :all_blank, :allow_destroy => true
   belongs_to :job
   belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
