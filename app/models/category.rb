b16f61d6913f78d25be9107048d0932834f74d91
Mustapha <ait.mus06@gmail.com>
2019-11-01 01:37:14 +0100

    add relation to workers and tags

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,9 @@ class Category < ApplicationRecord
   has_many :jobs, dependent: :destroy
   has_many :join_categories
   has_many :subcategories, :through => :join_categories
+  has_many :workers
+  has_many :join_tags
+  has_many :tags, through: :join_tags
   accepts_nested_attributes_for :subcategories
-  
+
 end
