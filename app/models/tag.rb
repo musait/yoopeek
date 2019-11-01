ea7e9de3ad74bca1cebcb58de339e9d448359bdc
Mustapha <ait.mus06@gmail.com>
2019-11-01 12:32:35 +0100

    add relation to join table tag worker

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,9 @@
 class Tag < ApplicationRecord
   has_many :join_tags
   has_many :categories, through: :join_tags
-  has_many :workers, through: :categories
+
+
+
+  has_many :join_tag_workers
+  has_many :workers, through: :join_tag_workers
 end
