216427577e1e91f1afce6c19f3d89e0652bb55ce
Cruxys <destin.ludovick@gmail.com>
2019-11-04 13:20:00 +0100

    CI: validates for check worker credits before create

--- app/models/room_message.rb
+++ app/models/room_message.rb
@@ -5,6 +5,13 @@ class RoomMessage < ApplicationRecord
   acts_as_readable on: :created_at
   has_one :notification
   after_create :create_notification
+  validate :check_author_credits
+
+  def check_author_credits
+    if author.worker?
+      errors.add "credits", I18n.t("not_enougth_credits") if room.room_messages.where(author_id: author.id).count > 0
+    end
+  end
 
   def create_notification
     Notification.create_for receiver, self
