5aad799c49d26ffe42a8a6208eacb35e1f788e3d
Mustapha <ait.mus06@gmail.com>
2019-11-05 11:43:34 +0100

    delete useless method

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ApplicationRecord
-  has_many :room_messages, dependent: :destroy, inverse_of: :room
+  has_many :room_messages, dependent: :destroy
   belongs_to :author, class_name: 'User'
   belongs_to :receiver, class_name: 'User'
   belongs_to :job
