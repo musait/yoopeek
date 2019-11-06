18a29cba5fbfe780fcc4edf6a3c5778985aff50d
Mustapha <ait.mus06@gmail.com>
2019-11-06 16:45:03 +0100

    add validations for content and price

--- app/models/quote_element.rb
+++ app/models/quote_element.rb
@@ -1,3 +1,5 @@
 class QuoteElement < ApplicationRecord
   belongs_to :quote
+  validates_numericality_of :price, presence: true, :message => :is_not_a_number
+  validates_presence_of :content, message: :must_be_present
 end
