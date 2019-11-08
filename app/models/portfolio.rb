521fa78179939d7902d73fcf9b1a520f1f5bab68
Cruxys <destin.ludovick@gmail.com>
2019-11-08 11:55:59 +0100

    add validates

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,7 +1,12 @@
 class Portfolio < ApplicationRecord
   belongs_to :user
   has_many_attached :picture
+  validate :check_user_plan
 
+  def check_user_plan
+    plan_limitation = user.current_plan
+    errors.add "plan", I18n.t("portfolio_limit_number", limit: plan_limitation.limit_portfolio_to_s, plan: plan_limitation.label) if( plan_limitation.limit_portfolio.present? &&plan_limitation.limit_portfolio <= 0)
+  end
   def picture_url
     if picture.attached?
       ActiveStorage::Current.set(host: Rails.application.credentials.dig(Rails.env.to_sym, :host)) do
