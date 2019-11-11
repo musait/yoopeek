77edc3f926dfeaf4246f748c9b566aa480261b07
Mustapha <ait.mus06@gmail.com>
2019-11-11 14:21:40 +0100

    add params for new_invoice params

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,38 +3,47 @@ class UserMailer < Devise::Mailer
 
 
     def new_quote
+      attachments.inline['image.png'] = File.read('public/theme/user/hireo/images/logo.png')
       @user = params[:user]
       @quote = params[:quote]
       mail(to: @user.email, subject: default_i18n_subject(user: @user.full_name))
     end
 
     def quote_declined
+      attachments.inline['image.png'] = File.read('public/theme/user/hireo/images/logo.png')
       @user = params[:user]
       @quote = params[:quote]
       mail(to: @user.email, subject: default_i18n_subject(user: @user.full_name))
     end
 
     def quote_paid
+      attachments.inline['image.png'] = File.read('public/theme/user/hireo/images/logo.png')
       @user = params[:user]
       @quote = params[:quote]
       mail(to: @user.email, subject: default_i18n_subject(user: @user.full_name))
     end
 
     def customer_declare_job_finished
+      attachments.inline['image.png'] = File.read('public/theme/user/hireo/images/logo.png')
       @user = params[:user]
       @job = params[:job]
       mail(to:@user.email,subject: default_i18n_subject(user: @user.full_name))
     end
     def worker_declare_job_finished
+      attachments.inline['image.png'] = File.read('public/theme/user/hireo/images/logo.png')
       @user = params[:user]
       @job = params[:job]
       mail(to:@user.email,subject: default_i18n_subject(user: @user.full_name))
     end
     def new_invoice
-      mail(to:Worker.first,subject: default_i18n_subject(user: Worker.first.full_name))
+      @user = params[:user]
+      @invoice = params[:invoice]
+      attachments.inline['image.png'] = File.read('public/theme/user/hireo/images/logo.png')
+      mail(to:@user,subject: default_i18n_subject(user: @user.full_name))
     end
 
     def subscription_expiration
+      attachments.inline['image.png'] = File.read('public/theme/user/hireo/images/logo.png')
       mail(to:Worker.first, subject: default_i18n_subject(user: Worker.first.full_name))
     end
   end
