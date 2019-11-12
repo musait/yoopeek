a08c467c5cac9726f27136cb96aa7b16f76cfa64
Mustapha <ait.mus06@gmail.com>
2019-11-12 16:38:04 +0100

    fix error message

--- app/validators/iban_validator.rb
+++ app/validators/iban_validator.rb
@@ -3,7 +3,7 @@ require 'iban-tools'
 class IbanValidator < ActiveModel::Validator
   def validate(record)
     unless IBANTools::IBAN.valid?(record.iban)
-      record.errors.add(:base, I18n.t("errors.messages.company.iban_not_valid"))
+      record.errors.add(:iban, I18n.t("errors.messages.company.iban_not_valid"))
     end
   end
 end
