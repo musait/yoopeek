a3aab1e87c958aa4e028c2f786e93d6d4c98e66f
Mustapha <ait.mus06@gmail.com>
2019-11-20 15:50:04 +0100

    delete binding.pry

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,6 @@ class PagesController < ApplicationController
   end
 
   def send_mail_contact
-    binding.pry
     UserMailer.with(sender: params[:email], name: params[:name], subject: params[:subject], content: params[:content]).send_mail_contact.deliver_now
     redirect_to '/pages/contact'
   end
