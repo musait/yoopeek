11f723e70e7ba58be811f506a2349830f87601e3
Mustapha <ait.mus06@gmail.com>
2019-11-25 16:10:45 +0100

    change redirect after edit

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -19,8 +19,12 @@ class RegistrationsController < Devise::RegistrationsController
     @subcategories = @profession.subcategories || Subcategory.all
     super
   end
+
   protected
 
+  def after_update_path_for(resource)
+    user_path(resource)
+  end
   def update_resource(resource, params)
     resource.update_without_password(params)
   end
