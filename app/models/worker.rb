b65ba0e57078295cb493cdbc2c4e1146f3f790cc
Mustapha <ait.mus06@gmail.com>
2019-11-25 16:10:24 +0100

    delete banner method

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -9,16 +9,9 @@ class Worker < User
 
   has_many :join_tag_workers
   has_many :tags, through: :join_tag_workers
-  has_one_attached :banner
 
-  def banner_url
-    if banner.attached?
-        ActiveStorage::Current.host = Rails.application.credentials.dig(Rails.env.to_sym, :host)
-        banner.service_url
-    else
-      ActionController::Base.helpers.asset_path 'logo.png'
-    end
-  end
+
+  
 
   def get_or_build_company
     company ||Company.new
