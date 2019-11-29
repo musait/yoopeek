ef2c1835d49f603c4da2aca9c69a3d85786f6383
Mustapha <ait.mus06@gmail.com>
2019-11-29 04:27:28 +0100

    add config for spam

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -1,4 +1,5 @@
 class DeviseMailer < Devise::Mailer
   layout 'mailer'
   default from: "contact@yoopeek.com"
+  default "Message-ID"=>"#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@yoopeek.com"
 end
