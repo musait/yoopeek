9ed0195279ba5876b30b067dc5d9d1cca359998c
Mustapha <ait.mus06@gmail.com>
2019-12-19 14:42:45 +0100

    add flash message

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,11 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!
   def show
-    render template: "pages/#{params[:page]}"
+    render template: "/pages/#{params[:page]}"
   end
 
   def send_mail_contact
     UserMailer.with(sender: params[:email], name: params[:name], subject: params[:subject], content: params[:content]).send_mail_contact.deliver_now
-    redirect_to '/pages/contact'
+    redirect_to '/pages/contact', notice: t('.the_email_has_been_sent')
   end
 end
